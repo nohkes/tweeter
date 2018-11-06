cd701af9f03cb189587322be126ae6dd5f4d6b68
Rawle Sekhon <amandeepsekhon404@gmail.com>
2018-11-06 21:16:41 +0000

    fixed some of the border and padding issues

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+var maxChar = 140;
+$(".new-tweet textarea").on('keyup', function(){
+var lengthOfInput = $(this).val().length;
+console.log(maxChar - lengthOfInput);
+var charLength = maxChar - lengthOfInput;
+if(charLength <= 0) {
+	$(this).siblings("span").text(charLength).css("color", "red");
+} else {
+	$(this).siblings("span").text(charLength);
+}
+});
+});
